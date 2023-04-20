c3726c7105c82e0192cc3672a5bb04d4f9ed7505
oc-garcia <octaviogarcia1988@gmail.com>
2023-04-20 10:35:22 -0300

    Scafolded back to JS

--- src/Hooks/ThemeContext/ThemeContext.ts
+++
@@ -1,8 +0,0 @@
-import { FC, ReactNode, createContext, useState } from "react";
-
-export const ThemeContext = createContext("light");
-
-export const ThemeProvider: React.FC<ReactNode> = ({ children }) => {
-  const [theme, setTheme] = useState("light");
-  return <ThemeContext.Provider value={(theme, setTheme)}>{children}</ThemeContext.Provider>;
-};
